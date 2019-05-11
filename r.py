45029ecb36997b5423d3b3b8b3e13b00d88b89ce
koreanlch <49800235+koreanlch@users.noreply.github.com>
2019-05-11 11:37:18 +0900

    Add files via upload

---
+++ r.py
@@ -0,0 +1,22 @@
+import re
+
+compiled = re.compile(r'([0-9]*)(N|kg|m|s|m\/s)')
+
+def prt_result(b, c):
+    result=int(b)/2
+    print(str(result)+str(c))
+
+def split_exec(obj):
+    a = compiled.search(obj)
+    if a != None:
+        b = a.group(1)
+        c = a.group(2) 
+        prt_result(b,c)
+    else:
+        print('올바른 값이 아닙니다. 올바른 값을 입력하세요.')
+
+print('주어진 값 : ',end="")
+received = input().split()
+
+for obj in received:
+    split_exec(obj)
